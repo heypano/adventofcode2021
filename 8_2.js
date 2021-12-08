389581eb7cf1556a454551eaa3d0b650984d8fb5
Pano Papadatos <pano@heypano.com>
2021-12-08 16:28:15 -0500

    progress

--- 8_2.js
+++ 8_2.js
@@ -20,16 +20,65 @@ const entries = input
     return entry.split(/\s*\|\s*/);
   });
 
+// console.log(digits);
+// console.log(letterMap);
+
 for (let entry of entries) {
   const [patterns, digitOutput] = entry;
-  const decodeMap = {};
-  const encodedDigits = digitOutput.trim().split(/\s+/);
+  const digitDecodeMap = {};
+  const potentialCharMap = {};
+  const encodedDigits = patterns.trim().split(/\s+/);
   encodedDigits.forEach((encodedDigit) => {
-    const uniqueDigit = uniqueDigitLengths[encodedDigit.length];
-    if (uniqueDigit) {
-      console.log(`${encodedDigit} is ${uniqueDigit}`);
+    const uniqueDigit = Number(uniqueDigitLengths[encodedDigit.length]);
+    if (Number.isInteger(uniqueDigit)) {
+      const actualEncoding = digits[uniqueDigit].segments;
+      console.log(uniqueDigit, encodedDigit, actualEncoding);
+      digitDecodeMap[encodedDigit] = uniqueDigit;
+      [...encodedDigit].forEach((encodedChar) => {
+        if (!potentialCharMap[encodedChar]) {
+          potentialCharMap[encodedChar] = {};
+          actualEncoding.forEach((actualEncodingChar) => {
+            potentialCharMap[encodedChar][actualEncodingChar] = true;
+          });
+          // encodedChar is b
+          // right now potentialCharMap[encodedChar] is { c: true, f: true }
+          // which other encoded digits contain `b` ?
+          const otherEncodedDigitsWithThisChar = encodedDigits.filter(
+            (d) => d.includes(encodedChar) && uniqueDigitLengths[d.length]
+          );
+          // otherEncodedDigitsWithThisChar.forEach((otherEncodedDigit) => {
+          //   const otherUniqueDigit = Number(
+          //     uniqueDigitLengths[otherEncodedDigit.length]
+          //   );
+          //   if (otherUniqueDigit) {
+          //   }
+          // });
+          console.log(
+            `Others with ${encodedChar}:`,
+            otherEncodedDigitsWithThisChar
+          );
+        }
+        // else {
+        //   const potentialChars = potentialCharMap[encodedChar];
+        //   Object.keys(potentialChars).forEach((potentialChar) => {
+        //     if (!encodedDigit.includes(potentialChar)) {
+        //       console.log(
+        //         `AHA! ${encodedChar} cannot possibly be ${potentialChar} since it's not in ${encodedDigit}`
+        //       );
+        //       delete potentialChars[potentialChar];
+        //     }
+        //   });
+        //   console.log(`${encodedChar} already has an entry`, potentialChars);
+        // }
+      });
+      // console.log("AAA", potentialCharMap);
+      // console.log("\n");
+      // console.log(`${encodedDigit} is ${uniqueDigit}`);
+    } else {
+      digitDecodeMap[encodedDigit] = null;
     }
   });
+  console.log(potentialCharMap);
   // console.log(encodedDigits);
   break;
 }
